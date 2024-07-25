6de7dc8a72fc365249eb4a5ccdbba41e88f03ccb
DmitriySolovyov <solovyov1986@mail.ru>
2024-07-25 11:37:08 +0300

    add babel, add extract plugin

---
+++ src/app/App.tsx
@@ -0,0 +1,27 @@
+import React, {Suspense, useContext, useState} from 'react';
+import {Link, Route, Routes} from 'react-router-dom';
+import './styles/index.scss';
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
+import {Sidebar} from "widgets/Sidebar";
+
+const App = () => {
+    const {theme} = useTheme();
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <Suspense fallback="">
+                <Navbar/>
+                <div className="content-page">
+                    <Sidebar/>
+                    <AppRouter/>
+                </div>
+            </Suspense>
+        </div>
+    );
+};
+
+export default App;
